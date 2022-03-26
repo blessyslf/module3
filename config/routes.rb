b4ed51b0335ab21f70186539db1ac4851bf1e009
Angelina1905 <gelyaros@yandex.ru>
2022-03-27 00:26:42 +0300

    Added h1 tag

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,31 @@
 Rails.application.routes.draw do
+  resources :categories
+  mount Ckeditor::Engine => '/ckeditor'
+  devise_for :users
+  get 'main/index'
+  get 'lending/index'
+  get 'styleguide/index'
+
+
+  resources :posts do
+      resources :comments
+      resources :likes
+      resources :favorites
+  end
+
+get 'users', to: 'users#index'
+get 'users/:id' => 'users#show', :as => :user
+delete 'users/:id', to: 'users#destroy'
+
+
+
+get 'about/index'
+get 'promo/index'
+
+  get 'posts/index'
+  get 'promo', to: 'promo#index'
+  get 'about', to: 'about#index'
+
+  root 'promo#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
